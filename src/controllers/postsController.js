78db84d22c39b213d2f07814e8051d646032daaa
Aleex1237 <alexis10893@outlook.com>
2021-10-21 17:56:09 -0300

    added error handler on route GET of posts

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -2,13 +2,19 @@ const db = require("../../database/models");
 
 module.exports = {
   posts: async (req, res) => {
-    let posts = await db.posts.findAll();
+    let posts = await db.Post.findAll();
 
     try {
-      return res.status(200).json({
-        status: `${200} ok`,
-        length: posts.length,
-        data: posts,
+      if (posts.length > 0) {
+        return res.status(200).json({
+          status: `${200} ok`,
+          length: posts.length,
+          data: posts,
+        });
+      }
+      return res.status(400).json({
+        status: `${400}`,
+        msg: `No hay posts para mostrar`,
       });
     } catch (error) {
       res.status(500).json({ status: `${500} error!`, error: error });
