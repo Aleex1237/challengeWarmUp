96bdd83e16922eb15657e332b210ce882a361baa
Aleex1237 <alexis10893@outlook.com>
2021-10-22 00:29:33 -0300

    adding enviroment variables to a config.jsfile

---
+++ database/config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: "challenge_warmup_db",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    port: process.env.PORT,
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
